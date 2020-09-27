9a8a13fc042b641c1f726f0b45afa517ad10f54f
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2020-09-27 22:11:29 +0200

    having fun with hovers

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
         <header className='mainHeader'>
-            <h1 className='logo'>Online Sampler</h1>
+            <Link to='/'><h1 className='logo'>Online Sampler</h1></Link>
             <p>Awake your creativity</p>
         </header>
     )
