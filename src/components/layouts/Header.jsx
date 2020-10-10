7022d552202c82b3fea677fa2e59347431ef9960
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2020-10-10 20:24:04 +0200

    Improved header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
+
         <header className='mainHeader'>
-            <Link to='/'><h1 className='logo'>Online Mellotron</h1></Link>
-            <p>Awake your creativity</p>
+            <Link to='/'>
+                <h1 className='logo logo--left'>Online</h1>
+                <h1 className='logo logo--right'>Mellotron </h1>
+            </Link>
+            <p>Awake creativity</p>
         </header>
     )
 }
