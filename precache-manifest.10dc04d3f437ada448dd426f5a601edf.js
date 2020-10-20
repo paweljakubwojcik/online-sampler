a35654e33fd4b595a239baf3c1dd29f0a8da2798
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2020-10-20 20:20:33 +0200

    Updates

---
+++ precache-manifest.10dc04d3f437ada448dd426f5a601edf.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f563a05260a73b58ad0cf1e11847ef1e",
+    "url": "/online-sampler/index.html"
+  },
+  {
+    "revision": "c90501fa444d2e9151d2",
+    "url": "/online-sampler/static/css/main.0184978d.chunk.css"
+  },
+  {
+    "revision": "629b0d9ea4ca66a41541",
+    "url": "/online-sampler/static/js/2.b6200399.chunk.js"
+  },
+  {
+    "revision": "714b9be1ce2a4274e31dbc552549e94d",
+    "url": "/online-sampler/static/js/2.b6200399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90501fa444d2e9151d2",
+    "url": "/online-sampler/static/js/main.257d35bb.chunk.js"
+  },
+  {
+    "revision": "5b726c7ec52d36fcc28a",
+    "url": "/online-sampler/static/js/runtime-main.270aab4b.js"
+  },
+  {
+    "revision": "3e6ae13073b6febac7818fdde7a60ed6",
+    "url": "/online-sampler/static/media/Ableton.3e6ae130.png"
+  },
+  {
+    "revision": "2c6ee5adf15e749a5ac1e5305bc4c279",
+    "url": "/online-sampler/static/media/Hero.2c6ee5ad.png"
+  },
+  {
+    "revision": "bf03e1a40801032b70b1327a9024163c",
+    "url": "/online-sampler/static/media/InTheMix.bf03e1a4.jpg"
+  },
+  {
+    "revision": "bdcf7f478c7fd3d5b370716d5826802f",
+    "url": "/online-sampler/static/media/Mellotron.bdcf7f47.png"
+  },
+  {
+    "revision": "de0295b1a4e949af07fd05f5b762ccc2",
+    "url": "/online-sampler/static/media/PadsSample.de0295b1.mp3"
+  },
+  {
+    "revision": "afa58736206034871fe977cce6dc9806",
+    "url": "/online-sampler/static/media/PianoSample.afa58736.mp3"
+  },
+  {
+    "revision": "02c75123f20c31a3d10c952b1fc390e3",
+    "url": "/online-sampler/static/media/Sanjay.02c75123.jpg"
+  },
+  {
+    "revision": "db30e431afe2c3a5233978e82396fa2f",
+    "url": "/online-sampler/static/media/StringsSample.db30e431.mp3"
+  },
+  {
+    "revision": "ae231a4f4fa5e253c9412306a70e413c",
+    "url": "/online-sampler/static/media/SynthSample.ae231a4f.mp3"
+  },
+  {
+    "revision": "8c0ac3743011351188e6a5752f7633ad",
+    "url": "/online-sampler/static/media/Teatro.8c0ac374.jpg"
+  },
+  {
+    "revision": "d2f7b662d8c538265b64f739e7dfa640",
+    "url": "/online-sampler/static/media/VocalSample.d2f7b662.mp3"
+  },
+  {
+    "revision": "435f6b718c839b550ee79590889ce733",
+    "url": "/online-sampler/static/media/andrew.435f6b71.jpg"
+  },
+  {
+    "revision": "d7282e404d49d4d3e7fcf29d0aee02c4",
+    "url": "/online-sampler/static/media/cassette-tape.d7282e40.svg"
+  },
+  {
+    "revision": "db9e9142d5d2098a58d7871207cfc9ca",
+    "url": "/online-sampler/static/media/mic.db9e9142.svg"
+  },
+  {
+    "revision": "3563bb743e1bdaf7fb465a76c2bf7a99",
+    "url": "/online-sampler/static/media/piano.3563bb74.svg"
+  },
+  {
+    "revision": "bddb70ecaa11eaf7be45c36772b56b93",
+    "url": "/online-sampler/static/media/react-logo.bddb70ec.svg"
+  },
+  {
+    "revision": "78f9cb09e22b4cd3fed265e9dc31e03c",
+    "url": "/online-sampler/static/media/sound-wave.78f9cb09.svg"
+  },
+  {
+    "revision": "6fa58b52d1d18b99f7c568cd46f8e66e",
+    "url": "/online-sampler/static/media/synth.6fa58b52.svg"
+  },
+  {
+    "revision": "3813a6985e9f20e664f336e7537425eb",
+    "url": "/online-sampler/static/media/violin.3813a698.svg"
+  }
+]);
