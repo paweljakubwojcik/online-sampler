912a9bb0b6630729470440a37c6b7152c9f0e0cb
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2020-10-22 00:18:07 +0200

    fixing orientation bug

--- src/globalMethods.js
+++ src/globalMethods.js
@@ -1,12 +1,17 @@
+import config from '../package.json'
+const rootAddres = config.homepage.split('/').pop()
+
+
 export const changeOrientation = () => {
 
+
     let path = document.location.pathname
-    
+    console.log(rootAddres, path)
 
     let html = document.querySelector('html')
     let body = document.querySelector('body')
     let app = document.querySelector('.App')
-    if (path === '/' && window.matchMedia('(orientation: portrait)').matches) {
+    if (path === `/${rootAddres}/` && window.matchMedia('(orientation: portrait)').matches) {
         html.style.transform = ' rotate(90deg)'
         body.style.width = '100vh'
         body.style.height = '100vw'
